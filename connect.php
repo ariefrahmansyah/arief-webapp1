312e83aaedbec192c76482fc6fc99d21a5c3628f
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2015-11-15 14:20:58 +0700

    Tambah connect.php & ubah hello.php

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+// sesuain dengan properties database di azure
+$hostname = "br-cdbr-azure-south-a.cloudapp.net";
+$username = "ba68174dc5f0d8";
+$password = "56b74823";
+$database_name = "db_webapp";
+
+$conn = mysql_connect($hostname, $username, $password) 
+		or die ("Error menghubungkan ke host database");
+$db = mysql_select_db($database_name) 
+	or die ("Error menghubungkan ke database");
+?>
